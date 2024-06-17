ee072a8ca3a5eb4943422bd369afc83cfcb00f72
Febrian Andi Nugroho <febrianandinugroho@gmail.com>
2024-06-17 12:19:47 +0700

    commit

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'
+import axios from 'axios'
+import { API_URL, API_KEY } from '@env';
+import { View, StyleSheet, ActivityIndicator, Text } from 'react-native'
+import WeatherSearch from './src/components/WeatherSearch'
+import WeatherInfo from './src/components/WeatherInfo'
 
-export default function App() {
+const App = () => {
+  const [weatherData, setWeatherData] = useState()
+  const [status, setStatus] = useState('')
+
+  const renderComponent = () => {
+    switch (status) {
+      case 'loading':
+        return <ActivityIndicator size="large" />
+      case 'success':
+        return <WeatherInfo weatherData={weatherData} />
+      case 'error':
+        return (
+          <Text style={styles.errorText}>
+            Something went wrong. Please try again with a correct city name.
+          </Text>
+        )
+      default:
+        return
+    }
+  }
+
+  const searchWeather = (location) => {
+    setStatus('loading')
+    axios
+      .get(`${API_URL}?q=${location}&appid=${API_KEY}`)
+      .then((response) => {
+        const data = response.data
+        // console.log(data)
+        data.visibility /= 1000
+        data.visibility = data.visibility.toFixed(2)
+        data.main.temp = (data.main.temp - 273.15).toFixed(2)
+        setWeatherData(data)
+        setStatus('success')
+      })
+      .catch((error) => {
+        // console.log(error)
+        setStatus('error')
+      })
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <WeatherSearch searchWeather={searchWeather}/>
+      <View style={styles.margintTop20}>{renderComponent()}</View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 20,
   },
-});
+  errorText: {
+    color: '#C60000',
+    fontSize: 16,
+    marginTop: 10,
+  },
+})
+
+export default App
